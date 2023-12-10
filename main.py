851c1418f9419b700f36c984eac5d189feaf6f7b
chris <christopher.neale@open.ac.uk>
2023-12-10 13:49:32 +0000

    Adding initial features database.

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_feature(feature_id):
+    conn = get_db_connection()
+    feature = conn.execute('SELECT * from features WHERE id = ?', (feature_id,)).fetchone()
+    conn.close()
+    if feature is None:
+        abort(404)
+    return feature
 
 @app.route('/')
 def index():
@@ -15,6 +29,23 @@ def papers():
 def tampering():
     return render_template('tampering.html')
 
+@app.route('/tampering_features')
+def tampering_features():
+    conn = get_db_connection()
+    features = conn.execute('SELECT * from features').fetchall()
+    conn.close()
+    return render_template('tampering_features.html', features=features)
+
+@app.route('/add_feature')
+def add_feature():
+    return render_template('add_feature.html')
+
+@app.route('/<int:feature_id>')
+def feature(feature_id):
+    feature = get_feature(feature_id)
+    return render_template('display_feature.html', feature=feature)
+
 if __name__==("__main__"):
-    #app.run(ssl_context='adhoc')
-    app.run()
+    app.run(ssl_context=('certs/cert.pem', 'certs/privkey.pem'))
+    #app.run(host='0.0.0.0', port='443', ssl_context=('certs/cert.pem', 'certs/privkey.pem                                                                   '))
+    #app.run()
