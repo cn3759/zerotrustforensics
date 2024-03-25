0701bed3422c2425c50709fd505e86d4b0d5a458
chris <christopher.neale@open.ac.uk>
2024-03-25 02:02:29 +0000

    Added details of tampering model

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 ids = ['AT-1', 'AT-2', 'AB-1', 'AB-2', 'AB-3', 'AB-4']
 
 def get_db_connection():
-    conn = sqlite3.connect('database_orig.db')
+    conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
